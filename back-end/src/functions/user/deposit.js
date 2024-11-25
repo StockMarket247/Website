d83973620aa3717142f523484071cb0aca166df7
Zeus-Adin <bollyseaglesway1@gmail.com>
2024-11-25 21:35:38 +0100

    updates

--- back-end/src/functions/user/deposit.js
+++
@@ -1,7 +0,0 @@
-const { initiateLogin } = require('./login');
-
-async function initiateDeposit() {
-    return await initiateLogin();;
-}
-
-module.exports = { initiateDeposit };
--- back-end/src/functions/user/login.js
+++
@@ -1,5 +0,0 @@
-async function initiateLogin(userInfo) {
-    return { userInfo:'Favours Head' };
-}
-
-module.exports = { initiateLogin };
